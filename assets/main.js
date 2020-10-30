bcb7a092fb88a3a6f85b425a0434152e2cca4e04
Anthony Keller <anthonyk3801@gmail.com>
2020-10-30 16:31:48 -0400

    Added crates in new room and options to pick one

--- assets/main.js
+++ assets/main.js
@@ -139,3 +139,55 @@ function yes(){
   document.getElementById("crate1").hidden = false;
   document.getElementById("crate2").hidden = false;
 }
+
+
+function crate1(){
+  document.getElementById("crate1").hidden = true;
+  document.getElementById("straight").hidden = true;
+  document.getElementById("openDoor").hidden = true;
+  document.getElementById("yes").hidden = true;
+  document.getElementById("no").hidden = true;
+  document.getElementById("yes2").hidden = true;
+  document.getElementById("yes3").hidden = true;
+  document.getElementById("num1").hidden = false;
+  document.getElementById("end").hidden = false;
+}
+
+function crate2(){
+  document.getElementById("openDoor").hidden = true;
+  document.getElementById("yes").hidden = true;
+  document.getElementById("no").hidden = true;
+  document.getElementById("yes2").hidden = true;
+  document.getElementById("yes3").hidden = true;
+  document.getElementById("num1").hidden = true;
+  document.getElementById("crate1").hidden = true;
+  document.getElementById("crate2").hidden = true;
+  document.getElementById("num2").hidden = false;
+  document.getElementById("num3").hidden = false;
+  document.getElementById("end").hidden = false;
+}
+
+function no(){
+  document.getElementById("nextRooms").hidden = true;
+  document.getElementById("left2").hidden = true;
+  document.getElementById("right2").hidden = true;
+  document.getElementById("straight").hidden = true;
+  document.getElementById("openDoor").hidden = true;
+  document.getElementById("yes").hidden = true;
+  document.getElementById("no").hidden = true;
+  document.getElementById("no2").hidden = false;
+  document.getElementById("end").hidden = false;
+}
+
+function dead(){
+  document.getElementById("text3").hidden = true;
+  document.getElementById("text4").hidden = true;
+  document.getElementById("class").hidden = true;
+  document.getElementById("start").hidden = true;
+  document.getElementById("Form").hidden = true;
+  document.getElementById("move").hidden = true;
+  document.getElementById("poke").hidden = true;
+  document.getElementById("moveDesk").hidden = true;
+  document.getElementById("pokePerson").hidden = false;
+  document.getElementById("end").hidden = false;
+}
