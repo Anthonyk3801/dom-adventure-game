e5a4e5f737d9ff4088845214e247b2906aa066b0
Anthony Keller <anthonyk3801@gmail.com>
2020-10-30 15:58:26 -0400

    Added start button and start function

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,20 @@
  * DOM Adventure Game
  */
 
-document.querySelector('#game').textContent = 'DOM Adventure Game';
+/*document.querySelector('#game').textContent = 'DOM Adventure Game';*/
+
+function start(){
+  document.getElementById("pokePerson").hidden = true;
+  document.getElementById("end").hidden = true;
+  document.getElementById("Form").hidden = true;
+  document.getElementById("move").hidden = true;
+  document.getElementById("poke").hidden = true;
+  document.getElementById("moveDesk").hidden = true;
+  document.getElementById("school").hidden = false;
+  document.getElementById("text").hidden = false;
+  document.getElementById("text2").hidden = false;
+  document.getElementById("start").hidden = true;
+  document.getElementById("left").hidden = false;
+  document.getElementById("middle").hidden = false;
+  document.getElementById("right").hidden = false;
+}
