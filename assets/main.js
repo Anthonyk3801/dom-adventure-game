99ec4fd8e550a16d68da5e9450163a07d9f2b54f
Anthony Keller <anthonyk3801@gmail.com>
2020-10-30 16:28:23 -0400

    Added yes or no options for opening basement door

--- assets/main.js
+++ assets/main.js
@@ -55,7 +55,7 @@ function clicksL(){
   document.getElementById("poke").hidden = false;
 }
 
-function next(){
+  function next(){
   document.getElementById("pokePerson").hidden = true;
   document.getElementById("text3").hidden = true;
   document.getElementById("text4").hidden = true;
@@ -106,5 +106,36 @@ function right(){
   document.getElementById("left2").hidden = true;
   document.getElementById("right2").hidden = true;
   document.getElementById("straight").hidden = false;
+}
+
+function straight(){
+  document.getElementById("move").hidden = true;
+  document.getElementById("poke").hidden = true;
+  document.getElementById("pokePerson").hidden = true;
+  document.getElementById("moveDesk").hidden = true;
+  document.getElementById("down").hidden = true;
+  document.getElementById("nextRooms").hidden = true;
+  document.getElementById("left2").hidden = true;
+  document.getElementById("right2").hidden = true;
+  document.getElementById("straight").hidden = true;
+  document.getElementById("openDoor").hidden = false;
   document.getElementById("yes").hidden = false;
   document.getElementById("no").hidden = false;
+}
+
+function yes(){
+  document.getElementById("pokePerson").hidden = true;
+  document.getElementById("moveDesk").hidden = true;
+  document.getElementById("down").hidden = true;
+  document.getElementById("nextRooms").hidden = true;
+  document.getElementById("left2").hidden = true;
+  document.getElementById("right2").hidden = true;
+  document.getElementById("straight").hidden = true;
+  document.getElementById("openDoor").hidden = true;
+  document.getElementById("yes").hidden = true;
+  document.getElementById("no").hidden = true;
+  document.getElementById("yes2").hidden = false;
+  document.getElementById("yes3").hidden = false;
+  document.getElementById("crate1").hidden = false;
+  document.getElementById("crate2").hidden = false;
+}
