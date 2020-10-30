20ef34efc966170ef9edc367ec78cc17849c035e
Anthony Keller <anthonyk3801@gmail.com>
2020-10-30 16:05:36 -0400

    Start game with spacebar and buttons in first room

--- assets/main.js
+++ assets/main.js
@@ -19,3 +19,20 @@ function start(){
   document.getElementById("middle").hidden = false;
   document.getElementById("right").hidden = false;
 }
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+      document.getElementById("start").hidden = true;
+      document.getElementById("end").hidden = true;
+      document.getElementById("Form").hidden = true;
+      document.getElementById("move").hidden = true;
+      document.getElementById("poke").hidden = true;
+      document.getElementById("moveDesk").hidden = true;
+      document.getElementById("school").hidden = false;
+      document.getElementById("text").hidden = false;
+      document.getElementById("text2").hidden = false;
+      document.getElementById("left").hidden = false;
+      document.getElementById("middle").hidden = false;
+      document.getElementById("right").hidden = false;
+    }
+}
