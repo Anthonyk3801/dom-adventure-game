860b1959d08bb52a79c93d08cf8efdc20b1aba84
Anthony Keller <anthonyk3801@gmail.com>
2020-10-30 16:08:46 -0400

    Created room for left option in start

--- assets/main.js
+++ assets/main.js
@@ -36,3 +36,21 @@ document.body.onkeyup = function(e){
       document.getElementById("right").hidden = false;
     }
 }
+
+function clicksL(){
+  document.getElementById("pokePerson").hidden = true;
+  document.getElementById("end").hidden = true;
+  document.getElementById("left").hidden = true;
+  document.getElementById("middle").hidden = true;
+  document.getElementById("right").hidden = true;
+  document.getElementById("school").hidden = true;
+  document.getElementById("text").hidden = true;
+  document.getElementById("text2").hidden = true;
+  document.getElementById("moveDesk").hidden = true;
+  document.getElementById("text3").hidden = false;
+  document.getElementById("text4").hidden = false;
+  document.getElementById("class").hidden = false;
+  document.getElementById("Form").hidden = false;
+  document.getElementById("move").hidden = false;
+  document.getElementById("poke").hidden = false;
+}
